d6d8f0f1eb356503d16a9fa8cb204882b2ba896a
Jiahan Xi <j7xi@uwaterloo.ca>
2021-04-11 02:34:02 -0400

    finished watcard & tested

---
+++ src/watcard.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+
+#include "headers/watcard.h"
+using namespace std;
+
+class WATCard::PImpl {
+    public:
+        unsigned int balance;
+        bool pop;
+    
+        PImpl() {
+            balance = 0;
+            pop = false;
+        }
+
+        ~PImpl() {}
+};
+
+void WATCard::markPaid() {
+    pimpl->pop = true;
+}
+
+WATCard::WATCard() : pimpl (new PImpl()) {}
+
+WATCard::~WATCard() {
+    delete pimpl;
+}
+
+void WATCard::deposit(unsigned int amount) {
+    pimpl->balance += amount;
+}
+
+
+void WATCard::withdraw(unsigned int amount) {
+    pimpl->balance -= amount;
+}
+
+unsigned int WATCard::getBalance() {
+    return pimpl->balance;
+}
+
+bool WATCard::paidForTicket() {
+    return pimpl->pop;
+}
+
+void WATCard::resetPOP() {
+    pimpl->pop = false;
+}
